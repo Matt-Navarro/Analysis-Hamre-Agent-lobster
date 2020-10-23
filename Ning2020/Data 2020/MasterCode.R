1572043c90ca0f8440f2ea7c757f65df7d988977
Nicole Hamre <hamrenicole@yahoo.co.uk>
2020-10-23 12:48:41 +0800

    Data merge

--- Ning2020/Data 2020/MasterCode.R
+++ Ning2020/Data 2020/MasterCode.R
@@ -4,31 +4,26 @@ is.data.frame(Meta) # checking object is df -> TRUE
 is.data.frame(Use) # checking object is df -> TRUE
 is.data.frame(Avidity) # checking object is df -> TRUE
 
-
-# To merge I need to connect:
-# Global ID of Meta --> GUID of USE
-# GlobalID of Use --> GUID of avidity
-
-# However have the same column names is going to confuse things so i'm going to change GlobalID of Use to GlobalUse and the GUID of avid to GUIDavidity
-# Therefore i'll be connecting:
-#GlobalID (Meta) --> GUID (Use)
-#GlobalUse (Use) --> GUIDavidity (avidity)
-
-## installing tidyverse library with dplyr
-install.packages("tidyverse")
-library(tidyverse)
-library(dplyr)
-
-Use %>% rename(GlobalUse = GlobalID) # Changing GlobalID to GlobalUse in Use df
-Avidity %>% rename(GUIDavid = Guid) # Changing GUID to GUIDavid in avidity df
-
-# The above section on changing the column names was actually unnessecesary
-# R changed the names of thecoumns with the same name automatically, but worth knowing how to reanme stuff. 
-
-
 MetaUse<- merge(Meta, Use, by.x = "GlobalID", 
                    by.y = "Guid", all.x = TRUE, all.y = TRUE) # merge Meta and Use dfs by GlobalID and GUID column
 MasterNing<- merge(MetaUse, Avidity, by.x = "GlobalID.y", 
                 by.y = "Guid", all.x = TRUE, all.y = TRUE) # merge MetaUse and Avidity dfs by GlobalUse and GUIDavid column
 
-write.csv(MasterNing, 'MasterNing.csv') # Need to fine tune the data on excel so saving as CSV
+write.csv(MasterNing, 'MasterNing.csv') # Need to fine tune the data on excel so saving as CSV in current workinmg directory. 
+
+## Merging SurveyType, SurveyType_two, Shift of MasterNing to Meta
+
+names(MasterNing) ## Checking names of variables
+
+install.packages("tidyverse")
+library(tidyverse)
+library(dplyr)
+
+DF <- MasterNing %>%
+  select(GlobalID.x, SurveyType, SurveyType_two, Shift) ##Subsetting using dplyr
+
+DF
+
+MetaED<-merge(Meta, DF, by.x='GlobalID', by.y='GlobalID.x')
+
+write.csv(MetaED, 'MetaED.csv')
